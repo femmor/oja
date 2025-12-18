eb5c000507145ab3e03f809a953614f3d8b395a8
Emmanuel Egomson <fegomson@gmail.com>
2025-12-18 14:52:51 -0600

    feat: setup cloudinary, inngest and clerk

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -8,6 +8,10 @@ const appConfig = {
     DB_URL: process.env.DB_URL || '',
     CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY || '',
     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY || '',
+    INNGEST_SIGNING_KEY: process.env.INNGEST_SIGNING_KEY || '',
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME || '',
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY || '',
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET || '',
 };
 
 // Validate required configurations
