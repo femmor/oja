78e2d0d4d7c358a66c7c1fddce1f90dbb9d0897e
Emmanuel Egomson <fegomson@gmail.com>
2025-12-22 18:21:42 -0600

    feat: Add order and customer management functionalities to admin controllers and routes

--- backend/src/routes/admin.route.ts
+++ backend/src/routes/admin.route.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { createProduct, deleteProduct, getAllProducts, updateProduct } from '../controllers/admin.controller';
+import { createProduct, deleteProduct, getAllCustomers, getAllOrders, getAllProducts, getDashboardStats, getOrderById, updateOrderStatus, updateProduct } from '../controllers/admin.controller';
 import { adminOnly, protectRoute } from '../middleware/auth.middleware';
 import upload from '../middleware/multer.middleware';
 
@@ -7,9 +7,19 @@ const router = Router();
 
 router.use(protectRoute, adminOnly);
 
+// Products routes
 router.post('/products', upload.array('images', 3), createProduct);
 router.get('/products', getAllProducts);
 router.put('/products/:id', upload.array('images', 3), updateProduct);
 router.delete('/products/:id', deleteProduct);
+
+//  Orders routes
+router.get('/orders', getAllOrders);
+router.get('/orders/:id', getOrderById);
+router.patch('/orders/:id/status', updateOrderStatus);
+
+// Customers routes:
+router.get('/customers', getAllCustomers);
+router.get('/stats', getDashboardStats);
 
 export default router;
